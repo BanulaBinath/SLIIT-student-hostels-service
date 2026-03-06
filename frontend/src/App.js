0cd68e6b6b291385b25e4035f7fb3748853f532c
BanulaBinath <banulabinath.edu@gmail.com>
2026-03-07 02:25:28 +0530

    fix the error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import NomalHome from './Components/Home/nomalhome';
 import Login from './Components/Home/login';
 import Register from './Components/Home/register';
 
 function App() {
   return (
-   
-      <div className="App">
-        
-        <React.Fragment>
-          
-            <Routes>
-              <Route path="/" element={<NomalHome />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          
-        </React.Fragment>
-       
+    <div className="App">
 
-      </div>
- 
+      <Routes>
+        <Route path="/" element={<NomalHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
+    </div>
   );
 }
 
