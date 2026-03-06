2aae48c7e2e9e9c903ea5bba2e04c51e0b0f89ec
BanulaBinath <banulabinath.edu@gmail.com>
2026-03-06 14:10:42 +0530

    create the file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
+import React from "react";
 import './App.css';
-import Home from './Components/Home';
+import { BrowserRouter as  Routes, Route } from "react-router-dom";
+import NomalHome from './Components/Home/nomalhome';
+import Login from './Components/Home/login';
+import Register from './Components/Home/register';
 
 function App() {
   return (
-    <div className="App">
-     <Home />
-    </div>
+   
+      <div className="App">
+        
+        <React.Fragment>
+          
+            <Routes>
+              <Route path="/" element={<NomalHome />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          
+        </React.Fragment>
+       
+
+      </div>
+ 
   );
 }
-
 
 export default App;
